ce66bcfdf0cb89dbc0c87c9158e631ec495a34a9
manibhaskar29 <kenguva.manibhaskar@gmail.com>
2025-12-25 16:28:37 +0530

    Protect routes using JWT-based authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Events from "./pages/Events";
 import Navbar from "./components/Navbar";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -11,9 +11,15 @@ function App() {
     <>
     <Navbar />
     <Routes>
+      <Route path="/" element={<Navigate to="/events" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/events" element={<Events />} />
+
+      <Route path="/events" element={
+        <ProtectedRoute>
+          <Events />
+        </ProtectedRoute>
+      } />
     </Routes>
     </>
   )
